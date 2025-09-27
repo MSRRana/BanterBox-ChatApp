c03358f292cf80ee69a064406ddc463cfa8736f1
Manish Rana <manish.rana@healiom.com>
2025-09-27 11:48:42 +0530

    Deploy ready code

--- backend/src/routes/messages.route.js
+++ backend/src/routes/messages.route.js
@@ -5,11 +5,5 @@ const router = express.Router();
 router.get("/send", (req, res) => {
   res.send("send a message");
 });
-router.get("/login", (req, res) => {
-  res.send("Login endpoint");
-});
-router.get("/logout", (req, res) => {
-  res.send("Logout endpoint");
-});
 
 export default router;
